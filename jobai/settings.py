7b64ff575ea4b8737a42bfbfe76213b9d5724495
sutharmohit575 <sutharmohit575@gmail.com>
2026-04-17 15:03:51 +0530

    fixed csrf completely

--- jobai/settings.py
+++ jobai/settings.py
@@ -3,19 +3,25 @@ import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-jobai-secret-key-change-in-production'
-DEBUG = True
-ALLOWED_HOSTS = ['.railway.app']
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jobai-secret-key-change-in-production')
+
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+# CSRF fix for Railway and custom domains
 CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app',
     'https://*.up.railway.app',
 ]
+RAILWAY_URL = os.environ.get('RAILWAY_PUBLIC_DOMAIN', '')
+if RAILWAY_URL:
+    CSRF_TRUSTED_ORIGINS += [f'https://{RAILWAY_URL}', f'http://{RAILWAY_URL}']
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+_extra_origins = os.environ.get('CSRF_TRUSTED_ORIGINS', '')
+if _extra_origins:
+    CSRF_TRUSTED_ORIGINS += [o.strip() for o in _extra_origins.split(',') if o.strip()]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -36,8 +42,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'jobai.urls'
@@ -74,7 +79,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
